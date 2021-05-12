c79cd7acaea0a75cba68399f63016d5ba789a43f
SarapMax <teerpong.me@gmail.com>
2021-05-13 00:15:01 +0700

    set up heroku server

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,9 +7,6 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - nuxtjs-frontend-template',
